7fe435941bdd94720d3a5efd2fe4810cb839ea54
Izer Onadim <izeronadim@gmail.com>
2023-05-20 21:04:58 +0100

    Add function for GC tracking choice mechanism

--- choice_mechanism.py
+++ choice_mechanism.py
@@ -1,3 +1,29 @@
 import random as rn
+from dna_mapping import bits_to_dna
+from utils import gc_content
 
 random_choice = lambda s, i, r: rn.choice(r)
+
+
+def gc_tracking(
+    state: str,
+    input: str,
+    reserved: list[str],
+    gc_target: float = 0.5,
+) -> str:
+    closest = None
+    least = 1
+
+    for bits in reserved:
+        concat = state + bits + input
+        gc = gc_content(bits_to_dna(concat))
+        diff = abs(gc_target - gc)
+
+        if diff == 0:
+            return bits
+
+        if diff < least:
+            least = diff
+            closest = bits
+
+    return closest
