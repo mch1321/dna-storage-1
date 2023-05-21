e980f3a10ca401aad8a2fd7cde784b18cc322954
Izer Onadim <izeronadim@gmail.com>
2023-05-21 18:28:13 +0100

    Run confusion experiment with GC tracking

--- conf.py
+++ conf.py
@@ -1,4 +1,5 @@
 import random as rn
+from choice_mechanism import gc_tracking
 from data_types import Path
 from dna_mapping import bits_to_dna, dna_to_bits
 from experiments import Parameters
@@ -78,13 +79,15 @@ if __name__ == "__main__":
     for er in [0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]:
         params = Parameters(
             sequence_length=300,
+            choice_mechanism=gc_tracking,
             error_rate=er,
             repetitions=10,
             random_seed=1704182,
         )
         conf = calc_confusion(params, verbose=True)
+        str_er = str(er).replace(".", "")
         plot_confusion(
-            f"er{er}-seq300-rep10-default",
-            f"Error Rate {er}, Symbol Length 4, 5 Reserved Bits, 0.35 < GC < 0.65",
+            f"er{str_er}-seq300-rep10-gc-tracking",
+            f"GC Tracking, Error Rate {er}, Symbol Length 4, 5 Reserved Bits, 0.35 < GC < 0.65",
             conf,
         )
