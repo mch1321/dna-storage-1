64712a2ad1b900bdfc41918f55180232145def9f
Izer Onadim <izeronadim@gmail.com>
2023-05-23 19:10:37 +0100

    Implement conv encoding and viterbi in Rust

--- viterbi/src/lib.rs
+++ viterbi/src/lib.rs
@@ -1,14 +1,43 @@
 use pyo3::prelude::*;
+mod conv;
 
-/// Formats the sum of two numbers as string.
+// Public types
+pub type FSM = (usize, usize, String, conv::Table);
+pub type Path = (usize, String, String);
+
+// Convolutional encoder.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn encode(fsm: FSM, msg: String) -> String {
+    return conv::conv(
+        &conv::FSM {
+            input_size: fsm.0,
+            output_size: fsm.1,
+            init_state: fsm.2,
+            table: fsm.3,
+        },
+        msg,
+    );
 }
 
-/// A Python module implemented in Rust.
+// Viterbi decoder.
+#[pyfunction]
+fn decode(fsm: FSM, msg: String) -> Path {
+    let path = conv::viterbi(
+        &conv::FSM {
+            input_size: fsm.0,
+            output_size: fsm.1,
+            init_state: fsm.2,
+            table: fsm.3,
+        },
+        msg,
+    );
+    return (path.length, path.sequence, path.observations);
+}
+
+/// A Python module for convolutional codes implemented in Rust.
 #[pymodule]
 fn viterbi(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(encode, m)?)?;
+    m.add_function(wrap_pyfunction!(decode, m)?)?;
     Ok(())
 }
