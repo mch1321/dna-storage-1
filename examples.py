a9575d34425326083e5360b49fd5528d3c04cb51
Izer Onadim <izeronadim@gmail.com>
2023-05-23 23:20:42 +0100

    Add examples using rust

--- examples.py
+++ examples.py
@@ -1,3 +1,7 @@
+import viterbi
+import encoding
+import numpy as np
+import random as rn
 from choice_mechanism import gc_tracking, random_choice
 from constraints import default_constraints
 from data_types import Path, Transition
@@ -18,7 +22,7 @@ EXAMPLE_MESSAGE = "001010101110010001010110110110100010111010100110011110010001"
 LONGER_MESSAGE = "001010101110010001010110110110100010111010100110011110010001001010101110010001010110110110100010111010100110011110010001"
 
 
-def constructing_transition_tables():
+def constructing_transition_tables(output=True):
     states = ["00", "01", "10", "11"]
     inputs = ["0", "1"]
 
@@ -46,7 +50,9 @@ def constructing_transition_tables():
     if from_list != from_matrix:
         raise Exception("Error found in transition table methods")
 
-    print(beautify_table(from_list))
+    if output:
+        print(beautify_table(from_list))
+    return from_list
 
 
 def one_half(eg=EXAMPLE_MESSAGE, output=True):
@@ -191,7 +197,7 @@ def dna_round_trip(eg=EXAMPLE_MESSAGE, output=True):
     return dna, enc, fsm
 
 
-def dna_round_trip_with_errors(eg=EXAMPLE_MESSAGE, error_rate=0.1, output=True):
+def dna_round_trip_with_errors(eg=EXAMPLE_MESSAGE, error_rate=0.05, output=True):
     dna, enc, fsm = dna_round_trip(eg, output=False)
     dna_err = inject_base_errors(dna, error_rate)
     err = dna_to_bits(dna_err)
@@ -248,7 +254,7 @@ def dna_gc_tracking(eg=EXAMPLE_MESSAGE, output=True):
     return dna, enc, fsm
 
 
-def dna_gc_tracking_with_errors(eg=EXAMPLE_MESSAGE, error_rate=0.1, output=True):
+def dna_gc_tracking_with_errors(eg=EXAMPLE_MESSAGE, error_rate=0.05, output=True):
     dna, enc, fsm = dna_gc_tracking(eg, output=False)
     dna_err = inject_base_errors(dna, error_rate)
     err = dna_to_bits(dna_err)
@@ -273,6 +279,140 @@ def dna_gc_tracking_with_errors(eg=EXAMPLE_MESSAGE, error_rate=0.1, output=True)
     return dna, fsm
 
 
+def rust_dna_round_trip(eg=EXAMPLE_MESSAGE, output=True):
+    symbol_size = 4
+    reserved_bits = 5
+
+    output_size = 2 * symbol_size
+    input_size = output_size - reserved_bits
+    init_state = output_size * "0"
+
+    c = default_constraints()
+    constraints = (c.gc_min, c.gc_max, c.max_run_length, c.reserved)
+    fsm = encoding.random_fsm(
+        symbol_size, reserved_bits, init_state, constraints, 123456
+    )
+
+    enc = viterbi.encode(fsm, eg)
+    dna = encoding.bits_to_dna(enc)
+    (l, seq, obs) = viterbi.decode(fsm, encoding.dna_to_bits(dna))
+
+    if output:
+        print("RUST DNA ROUND TRIP WITHOUT ERRORS")
+
+        print(f"Raw message        : {eg}")
+        print(f"Encoded message    : {enc}")
+        print(f"DNA message        : {dna}")
+        print(f"Viterbi path       : {obs}")
+        print(f"Decoded message    : {seq}")
+        print(f"DNA message correct: {dna == encoding.bits_to_dna(obs)}")
+        print(f"Found correct path : {enc == obs}")
+        print(f"Correctly decoded  : {eg == seq}")
+        print(f"GC Content         : {gc_content(dna)}")
+        print("----------------------------------------------------")
+
+    return dna, enc, fsm
+
+
+def rust_dna_round_trip_with_errors(eg=EXAMPLE_MESSAGE, error_rate=0.05, output=True):
+    dna, enc, fsm = rust_dna_round_trip(eg, output=False)
+    dna_err = inject_base_errors(dna, error_rate)
+    err = encoding.dna_to_bits(dna_err)
+    (l, seq, obs) = viterbi.decode(fsm, err)
+    dna_cor = encoding.bits_to_dna(obs)
+
+    if output:
+        print(f"RUST DNA ROUND TRIP WITH ERROR RATE {error_rate}")
+        print(f"Raw message      : {eg}")
+        print(f"Encoded message  : {enc}")
+        print(f"DNA message      : {dna}")
+        print(f"Received message : {dna_err}")
+        print(f"Corrected DNA    : {dna_cor}")
+        print(f"Viterbi path     : {obs}")
+        print(f"Decoded message  : {seq}")
+        print(f"Errors injected  : {hamming_dist(enc, err)}")
+        print(f"Errors remaining : {hamming_dist(enc, obs)}")
+        print(f"After decoding   : {hamming_dist(eg, seq)}")
+        print(f"GC Content       : {gc_content(dna)}")
+        print("----------------------------------------------------")
+
+    return dna, fsm
+
+
+def rust_python_equivalence(eg=EXAMPLE_MESSAGE, error_rate=0.05, output=True):
+    python_table = constructing_transition_tables(output=False)
+    rust_table = {
+        "00": {"0": ("00", "00"), "1": ("10", "11")},
+        "01": {"0": ("00", "11"), "1": ("10", "00")},
+        "10": {"0": ("01", "01"), "1": ("11", "10")},
+        "11": {"0": ("10", "10"), "1": ("11", "01")},
+    }
+
+    py_fsm = FSM("00", python_table, 1, 2)
+    rust_fsm = (1, 2, "00", rust_table)
+
+    py_enc = py_fsm.conv(eg)
+    rust_enc = viterbi.encode(rust_fsm, eg)
+
+    assert rust_enc == py_enc
+    err = inject_bit_errors(rust_enc, error_rate)
+
+    py_dec = py_fsm.viterbi(err)
+    (l, seq, obs) = viterbi.decode(rust_fsm, err)
+
+    if output:
+        print(eg)
+        print(py_dec.sequence)
+        print(seq)
+        print(hamming_dist(py_dec.sequence, seq))
+        print(hamming_dist(eg, py_dec.sequence))
+        print(hamming_dist(eg, seq))
+    return hamming_dist(py_dec.sequence, eg), hamming_dist(seq, eg)
+
+
+def dna_rust_python_equivalence(eg=EXAMPLE_MESSAGE, error_rate=0.05, output=True):
+    symbol_size = 4
+    reserved_bits = 5
+
+    output_size = 2 * symbol_size
+    input_size = output_size - reserved_bits
+    init_state = output_size * "0"
+
+    c = default_constraints()
+    constraints = (c.gc_min, c.gc_max, c.max_run_length, c.reserved)
+    rs_fsm = encoding.random_fsm(
+        symbol_size, reserved_bits, init_state, constraints, 3928401
+    )
+
+    py_fsm = construct_fsm_from_constraints(
+        init_state, input_size, output_size, c, random_choice
+    )
+
+    py_enc = py_fsm.conv(eg)
+    py_dna = bits_to_dna(py_enc)
+
+    rs_enc = viterbi.encode(rs_fsm, eg)
+    rs_dna = encoding.bits_to_dna(rs_enc)
+
+    py_dna_err = inject_base_errors(py_dna, error_rate)
+    py_err = dna_to_bits(py_dna_err)
+    rs_dna_err = inject_base_errors(rs_dna, error_rate)
+    rs_err = encoding.dna_to_bits(rs_dna_err)
+
+    py_path = py_fsm.viterbi(py_err)
+    (l, seq, obs) = viterbi.decode(rs_fsm, rs_err)
+
+    if output:
+        print(eg)
+        print(py_path.sequence)
+        print(seq)
+        print(hamming_dist(py_path.sequence, seq))
+        print(hamming_dist(eg, py_path.sequence))
+        print(hamming_dist(eg, seq))
+
+    return hamming_dist(eg, py_path.sequence), hamming_dist(eg, seq)
+
+
 def run_all_examples():
     constructing_transition_tables()
     one_half()
@@ -280,10 +420,14 @@ def run_all_examples():
     two_thirds()
     two_thirds_with_errors()
     dna_round_trip()
-    dna_round_trip_with_errors()
+    dna_round_trip_with_errors(error_rate=0.05)
     dna_round_trip_with_errors(eg=rand_bit_string(90))
     dna_gc_tracking()
     dna_gc_tracking_with_errors()
+    rust_dna_round_trip()
+    rust_dna_round_trip_with_errors(error_rate=0.05)
+    rust_python_equivalence()
+    dna_rust_python_equivalence()
 
 
 if __name__ == "__main__":
