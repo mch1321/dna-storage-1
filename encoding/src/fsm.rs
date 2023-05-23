b59017e2d44ac4d98a4cfcf0d32c7edd1182f38b
Izer Onadim <izeronadim@gmail.com>
2023-05-23 19:20:25 +0100

    Reimplement FSM construction with Rust

---
+++ encoding/src/fsm.rs
@@ -0,0 +1,73 @@
+use crate::constraints::Constraints;
+use crate::mapping::bits_to_dna;
+use core::panic;
+use std::collections::HashMap;
+
+pub(crate) type FSM = (usize, usize, String, Table);
+type Table = HashMap<String, HashMap<String, (String, String)>>;
+
+fn populate_space(size: usize) -> Vec<String> {
+    let mut space: Vec<String> = Vec::new();
+    let dim = usize::pow(2, size as u32);
+    for i in 0..dim {
+        let bin = format!("{i:b}");
+        let padding = size - bin.len();
+        space.push("0".repeat(padding) + &bin);
+    }
+    return space;
+}
+
+fn construct_table(
+    input_size: usize,
+    output_size: usize,
+    constraints: Constraints,
+    mechanism: impl Fn(&str, &str, Vec<String>) -> String,
+) -> Table {
+    assert!(input_size < output_size);
+
+    let mut table: Table = HashMap::new();
+
+    let reserved_size = output_size - input_size;
+    let states = populate_space(output_size);
+    let inputs = populate_space(input_size);
+    let reserved = populate_space(reserved_size);
+
+    for s in &states {
+        table.insert(s.clone(), HashMap::new());
+        for i in &inputs {
+            let mut candidates: Vec<String> = Vec::new();
+            for r in &reserved {
+                if constraints.satisfied(bits_to_dna(&(s.to_owned() + r + i))) {
+                    candidates.push(r.to_string());
+                }
+            }
+            if candidates.is_empty() {
+                panic!("Impossible to meet constraints!");
+            }
+
+            let chosen = mechanism(&s, &i, candidates);
+            let output = chosen + &i;
+            table
+                .get_mut(s)
+                .unwrap()
+                .insert(i.to_string(), (output.clone(), output));
+        }
+    }
+
+    return table;
+}
+
+pub(crate) fn construct_fsm(
+    input_size: usize,
+    output_size: usize,
+    init_state: String,
+    constraints: Constraints,
+    mechanism: impl Fn(&str, &str, Vec<String>) -> String,
+) -> FSM {
+    return (
+        input_size,
+        output_size,
+        init_state,
+        construct_table(input_size, output_size, constraints, mechanism),
+    );
+}
--- encoding/src/lib.rs
+++ encoding/src/lib.rs
@@ -1,14 +1,109 @@
+use mechanisms::{gc_tracked_random, gc_tracking, random_choice};
 use pyo3::prelude::*;
+mod constraints;
+mod fsm;
+mod mapping;
+mod mechanisms;
 
-/// Formats the sum of two numbers as string.
+pub type Constraints = (f32, f32, usize, Vec<String>);
+
+// Converts nucleotides to bits.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn dna_to_bits(seq: &str) -> String {
+    return mapping::dna_to_bits(seq);
 }
 
-/// A Python module implemented in Rust.
+// Converts bits to nucleotides.
+#[pyfunction]
+fn bits_to_dna(seq: &str) -> String {
+    return mapping::bits_to_dna(seq);
+}
+
+// Uses the random choice mechanism to construct an FSM given constraints.
+#[pyfunction]
+fn random_fsm(
+    symbol_size: usize,
+    reserved_bits: usize,
+    init_state: String,
+    constraints: Constraints,
+    seed: u64,
+) -> fsm::FSM {
+    return generate_fsm(
+        symbol_size,
+        reserved_bits,
+        init_state,
+        constraints,
+        |_, _, r| random_choice(r, seed),
+    );
+}
+
+// Uses the GC-tracking mechanism to construct an FSM given constraints.
+#[pyfunction]
+fn gc_tracking_fsm(
+    symbol_size: usize,
+    reserved_bits: usize,
+    init_state: String,
+    constraints: Constraints,
+) -> fsm::FSM {
+    return generate_fsm(
+        symbol_size,
+        reserved_bits,
+        init_state,
+        constraints,
+        gc_tracking,
+    );
+}
+
+// Uses the GC-tracked Random mechanism to construct an FSM given constraints.
+#[pyfunction]
+fn gc_tracked_random_fsm(
+    symbol_size: usize,
+    reserved_bits: usize,
+    init_state: String,
+    constraints: Constraints,
+    seed: u64,
+) -> fsm::FSM {
+    return generate_fsm(
+        symbol_size,
+        reserved_bits,
+        init_state,
+        constraints,
+        |s, i, r| gc_tracked_random(s, i, r, seed),
+    );
+}
+
+// Constructs an FSM given constraints and a choice mechanism.
+fn generate_fsm(
+    symbol_size: usize,
+    reserved_bits: usize,
+    init_state: String,
+    constraints: Constraints,
+    mechanism: impl Fn(&str, &str, Vec<String>) -> String,
+) -> fsm::FSM {
+    let output_size = symbol_size * 2;
+    let input_size = output_size - reserved_bits;
+
+    return fsm::construct_fsm(
+        input_size,
+        output_size,
+        init_state,
+        constraints::Constraints {
+            gc_min: constraints.0,
+            gc_max: constraints.1,
+            max_run_length: constraints.2,
+            reserved: constraints.3,
+        },
+        mechanism,
+    );
+}
+
+/// A Python for FSM-based encodings implemented in Rust.
 #[pymodule]
 fn encoding(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(dna_to_bits, m)?)?;
+    m.add_function(wrap_pyfunction!(bits_to_dna, m)?)?;
+    m.add_function(wrap_pyfunction!(random_fsm, m)?)?;
+    m.add_function(wrap_pyfunction!(gc_tracking_fsm, m)?)?;
+    m.add_function(wrap_pyfunction!(gc_tracked_random_fsm, m)?)?;
     Ok(())
 }
